97e274cece20240dd73a6cea9445350066d279e6
John Cedeño <jjjohncedeno@gmail.com>
2016-05-31 23:58:30 -0500

    Añadiendo función de invertir cadena

--- Proyecto.s
+++ Proyecto.s
@@ -1,10 +1,10 @@
 
 
-        .data
+.data
         linea1:   .asciiz   "La palabra real es: "
         msg:      .asciiz   "Hola"
         linea2:   .asciiz   "\nLa palabra final: "
-
+        msgNuevo: .space    100
         .extern foobar 4
 
         .text
@@ -21,9 +21,36 @@ main:
         la      $a0, linea2
         syscall
 
+        la $a0, msg
+        la $a1, msgNuevo
+        jal invierte
+
         la      $a0, msg
         syscall
-        la      $a0, msg
+        la      $a0, msgNuevo
         syscall
-        
+
+
+         li $v0, 10 
+         syscall
         jr $ra          # retrun to caller
+
+invierte:
+        li $t1, 0   #Contador de la longitud
+        bucle:
+             lb $t0, ($a0)
+             beqz $t0, desapila
+             addi $sp, $sp, -1
+             sb $t0, ($sp)
+             addi $t1, $t1, 1
+             addi $a0, $a0, 1
+             j bucle
+        desapila:
+           lb $t0, ($sp)
+           addi $sp, $sp, 1
+           sb $t0, ($a1)
+           addi $a1, $a1, 1
+           addi $t1, $t1, -1
+           bnez $t1, desapila
+         sb $zero, ($a1)
+         jr $ra
